68d92f08ca8ca88660c5e15b017a8db5916c2dfb
kemi-p <kpole@dvtsoftware.com>
2026-04-15 00:13:33 +0200

    configure route for orders, add the orders component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: '',
     loadComponent: () =>
-      import('./home/home').then(m => m.HomeComponent),
+      import('./home/home').then(m => m.HomeComponent), 
+  },
+  {
+    path: 'order',
+    loadComponent: () =>
+      import('./order/order').then(m => m.OrderComponent),
   },
 ];
---
+++ src/app/order/order.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'order',
+  imports: [],
+  templateUrl: './order.html'
+})
+export class OrderComponent {
+
+}
